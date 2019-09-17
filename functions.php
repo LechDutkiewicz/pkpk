6680c8910b90b1b3909438f9ce382c0ac349c607
LechDutkiewicz <lechdutkiewicz@gmail.com>
2019-09-17 14:18:13 +0200

    dodany autozapis formularza z raportem

--- functions.php
+++ functions.php
@@ -54,7 +54,7 @@ array_map(function ($file) use ($sage_error) {
     if (!locate_template($file, true, true)) {
         $sage_error(sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file), 'File not found');
     }
-}, ['helpers', 'setup', 'filters', 'admin', 'post-types', 'metaboxes', 'rest', 'course-app', 'make-pdf', 'lib/edd-overrides/tpayLibs/src/_class_tpay/PaymentForms/pkpkpaymentform', 'pkpk-edd', 'shortcodes']);
+}, ['helpers', 'setup', 'filters', 'admin', 'post-types', 'metaboxes', 'rest', 'course-app', 'make-pdf', 'lib/edd-overrides/tpayLibs/src/_class_tpay/PaymentForms/pkpkpaymentform', 'pkpk-edd', 'shortcodes', 'ajax-actions']);
 
 /**
  * Here's what's happening with these hooks:
---
+++ src/ajax-actions.php
@@ -0,0 +1,48 @@
+<?php
+
+function autosave_raport() {
+
+	if ( !isset( $_POST ) || empty( $_POST ) ) {
+		$return_data['value'] = __( 'Cannot send email to destination. No parameter receive form AJAX call.', 'bon' );
+		die( json_encode( $return_data ) );
+
+	} else {
+
+		$lesson_id = $_POST['post_id'];
+		$userID = $_POST['user_id'];
+
+		// pobierz raporty dla danej lekcji
+		$reports = get_post_meta( $lesson_id, 'prod_userreporting_reports', true );
+
+		if ( $lesson_id ) {
+
+			$fields = get_post_meta( $lesson_id, 'prod_userreporting_fields', true );
+
+			// zapisz do BD wartości pól z raportu
+			if ($fields) {
+				foreach ($fields as $name => $field0) {
+					if ($field['type'] == 'longtext') {
+						$reportEntry[$name] = sanitize_textarea_field($_POST[$name]);
+					} else {
+						$reportEntry[$name] = sanitize_text_field($_POST[$name]);
+					}
+				}
+			}
+
+			// dodaj informację, że jest to wersja robocza
+			$reportEntry['wersja'] = 'robocza';
+
+			$reports[$userID] = $reportEntry;
+			update_post_meta( $lesson_id, 'prod_userreporting_reports', $reports) ;
+
+		}
+		die();
+
+	}
+
+}
+
+add_action( 'wp_ajax_autosave-raport', 'autosave_raport' );
+add_action( 'wp_ajax_nopriv_autosave-raport', 'autosave_raport' );
+
+?>
